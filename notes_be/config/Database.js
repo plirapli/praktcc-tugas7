faa788a020cdbf8df8f51e983399ff4ae0f469ee
plirapli <rafliyulistiawan@gmail.com>
2025-05-23 23:23:41 +0700

    tesss

--- notes_be/config/Database.js
+++ notes_be/config/Database.js
@@ -1,9 +1,7 @@
 import { Sequelize } from "sequelize";
 import "dotenv/config";
 
-console.log(
-  `tesss: ${process.env.DB_HOST}, ${process.env.DB_NAME}, ${process.env.DB_USERNAME}, ${process.env.DB_PASSWORD}`
-);
+console.log(Object.keys(process.env));
 
 const db = new Sequelize("tugas_notes", "root", "inipassword", {
   host: "104.154.138.53",
