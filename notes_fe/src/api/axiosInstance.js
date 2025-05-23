63f35400e11610c06f751a588cc9494f49868808
Jeslyn V <162588624+midnightwalks@users.noreply.github.com>
2025-05-23 21:02:21 +0700

    Update axiosInstance.js

--- notes_fe/src/api/axiosInstance.js
+++ notes_fe/src/api/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://notes-backend-598740328533.us-central1.run.app/", // Ganti dengan base URL backend-mu
+  baseURL: "https://notes-backend-598740328533.us-central1.run.app", // Ganti dengan base URL backend-mu
   withCredentials: true, // untuk kirim cookie kalau dibutuhkan
 });
 
