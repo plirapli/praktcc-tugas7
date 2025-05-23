55cb668fdc0402efe8f2097aaec3737acf403edd
plirapli <rafliyulistiawan@gmail.com>
2025-05-23 23:28:31 +0700

    ssssasasaasa

--- notes_be/config/Database.js
+++ notes_be/config/Database.js
@@ -1,7 +1,14 @@
 import { Sequelize } from "sequelize";
 import "dotenv/config";
 
-console.log(Object.keys(process.env));
+Object.keys(process.env).forEach((val) => {
+  console.log(process.env[val]);
+});
+
+console.log(process.env["DB_HOST"]);
+console.log(process.env["DB_NAME"]);
+console.log(process.env["DB_USERNAME"]);
+console.log(process.env["DB_PASSWORD"]);
 
 const db = new Sequelize("tugas_notes", "root", "inipassword", {
   host: "104.154.138.53",
